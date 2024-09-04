d6e972e8cc149b59e3c24bfd44704fe028c44fef
graindstone <bannah.chiu@gmail.com>
2024-09-04 16:13:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function nothings(){
+  document.getElementById("text").style.display="none";
+  document.getElementById("text").innerHTML="Like I said, there's nothing here. Nothing but a useless button.";
+  document.getElementById("text").style.display="block";
+  document.getElementById("button").style.display="none";
+  document.getElementById("waffle").style.display="block";
+}
