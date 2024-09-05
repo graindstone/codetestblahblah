9a407803164f54251207435224919e49d2f83b8e
graindstone <bannah.chiu@gmail.com>
2024-09-04 17:02:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ function nothings(){
   document.getElementById("button").style.display="none";
   document.getElementById("waffle").style.display="block";
 }
+let game = {
+  waffle: 0,
+  machine: 0,
+}
+function waffle(){
+  game.waffle++;
+}
+function startgame(){
+  document.getElementById("text").style.display="none";
+  document.getElementById("text").innerHTML="Wow! Crazy!";
+  document.getElementById("text").style.display="block";
+  waffle();
+}
+
